116bd85bd5d11f6624f038063f34e6d8d26b99df
frances harlow <frances.harlow@gmail.com>
2015-12-07 09:39:38 -0500

    debugging deployment

--- db/config.rb
+++ db/config.rb
@@ -2,10 +2,25 @@ require "active_record"
 
 # ActiveRecord::Base.logger = Logger.new(STDERR)
 
-ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database => 'db/database.sqlite3'
-)
+
+
+if ENV['RACK_ENV'] == "production"
+    require 'uri'
+    db = URI.parse(ENV['DATABASE_URL'])
+    ActiveRecord::Base.establish_connection({
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    })
+else
+  ActiveRecord::Base.establish_connection(
+    :adapter  => 'sqlite3',
+    :database => 'db/database.sqlite3'
+  )
+end
 
 Dir.glob("models/*.rb").each do |path|
   require_relative "../#{path}"
